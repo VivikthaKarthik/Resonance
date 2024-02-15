83b17be2b899a3a253e17aaeeba793d6ff9a2744
Vamsi <VivikthaKarthik@gmail.com>
2024-02-16 02:46:01 +0530

    Updated  version

--- Resonance/Controllers/HealthCheckController.cs
+++ Resonance/Controllers/HealthCheckController.cs
@@ -12,7 +12,7 @@ namespace ResoClassAPI.Controllers
         [HttpGet]
         public string Get()
         {
-            return "ResoClass API is Running successfully\nVersion: 9";
+            return "ResoClass API is Running successfully\nVersion: 10";
         }
     }
 }
