1bb78ed04399545aca5c315b68e285c849d1bb5e
Vamsi <VivikthaKarthik@gmail.com>
2024-03-23 23:57:20 +0530

    Fixed Chapter Issue

--- Resonance/DTOs/ChapterResponseDto.cs
+++ Resonance/DTOs/ChapterResponseDto.cs
@@ -8,6 +8,10 @@
         public string Thumbnail { get; set; } = null!;
         public bool IsRecommended { get; set; }
         public long SubjectId { get; set; }
-        public string SubjectName { get; set; }
+        public string Subject { get; set; }
+        public long CourseId { get; set; }
+        public string Course { get; set; }
+        public long ClassId { get; set; }
+        public string Class { get; set; }
     }
 }
